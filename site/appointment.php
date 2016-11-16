1928ad8521dad83f51fa14114c98dafca6bf8da0
FScholsen <fscholsen@gmail.com>
2016-11-16 20:17:18 +0100

    rdv form basic

--- site/appointment.php
+++ site/appointment.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: Rom
- * Date: 12/11/2016
- * Time: 18:53
+ * User: Francois
+ * Date: 15/11/2016
+ * Time: 12:53
  */
 session_start();
 include '../php/Fonctions.php';
@@ -31,54 +31,46 @@ include '../php/Fonctions.php';
     <link href="styles/custom.css" rel="stylesheet" type="text/css" />
     <script src="scripts/jquery.min.js"></script>
     <style>
+        #searchIcon{
+            width:3%;
+            margin-bottom: .8%;
+        }
+        #searchIcon:focus{
+            outline: none;
+        }
+        #suggestions{
+            margin-top: 5%;
+            display: none;
+        }
     </style>
     <script>
+        function showHint(hint){
+            $.get("../php/getHint.php?q=" + hint, function (data) {
+                if(data) {
+                    $('#suggestions').remove();
+                    $('#searchIcon').after('<div id="suggestions"></div>');
+                    $('#suggestions').fadeIn().html('<h2>Suggestions:</h2></br>' + data);
+                }else{
+                    $('#suggestions').fadeOut();
+                    $('#suggestions').remove();
+
+                }
+            });
+        }
+        function getResults(){
+            event.preventDefault();
+            $('#suggestions').remove();
+            var requete = $('#searchBar').val();
+            $.get("../php/traiteForm.php?rq=search&requete="+requete,function(data){
+                $('#searchIcon').after('<div id="suggestions"></div>');
+                $('#suggestions').fadeIn().html(data);
+            });
+        }
     </script>
 
 </head>
 <body>
-<div id="decorative2">
     <div class="container">
 
-        <div class="divPanel topArea notop nobottom">
-            <div class="row-fluid">
-                <div class="span12">
-
-                    <div id="divLogo" class="pull-left">
-                        <a href="index.php" id="divSiteTitle">E.W.R</a><br />
-                        <a href="index.php" id="divTagLine">Easy Waiting Room</a>
-                    </div>
-
-                    <div id="divMenuRight" class="pull-right">
-                        <div class="navbar">
-                            <button type="button" class="btn btn-navbar-highlight btn-large btn-primary" data-toggle="collapse" data-target=".nav-collapse">
-                                NAVIGATION <span class="icon-chevron-down icon-white"></span>
-                            </button>
-                            <div class="nav-collapse collapse">
-                                <ul class="nav nav-pills ddmenu">
-                                    <?php echo genereMenu('rendez-vous') ?>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="breadcrumbs">
-                    <a href="index.html">Home</a> &nbsp;/&nbsp; <span>Rendez-Vous</span>
-                </div>
-                <div id="textSearch">
-                    <p style ='text-align:center' ><h1 style ='text-align:center' >Recherche du professionel</h1></p>
-                </div>
-                <div id="searchBarDiv">
-                    <input type="text" id="searchBar" placeholder="recherche..." onkeyup="showHint(this.value)"/><input type="image" id="searchIcon" src="images/iconLoupe.png" name="mon_image" onclick="getResults()"/>
-                </div>
-
-                <script src="scripts/jquery.min.js" type="text/javascript"></script>
-                <script src="scripts/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-                <script src="scripts/default.js" type="text/javascript"></script>
-
-
-                <script src="scripts/carousel/jquery.carouFredSel-6.2.0-packed.js" type="text/javascript"></script><script type="text/javascript">$('#list_photos').carouFredSel({ responsive: true, width: '100%', scroll: 2, items: {width: 320,visible: {min: 2, max: 6}} });</script>
-
+    </div>
 </body>
-
-</html>
