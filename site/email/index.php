b3bb75a3c308e0d6c7ff831664da80579b628c6a
Martin G <martinouh@localhost.localdomain>
2016-12-06 11:09:30 +0100

    contact

--- site/email/index.php
+++ site/email/index.php
@@ -6,7 +6,7 @@
 	$query = $_POST['message'];
 	$email_from = $name.'<'.$email.'>';
 
- $to="contact@easywaitingroom.be";
+ $to="martinouh@gmail.com";
  $subject="Enquiry!";
  $headers  = 'MIME-Version: 1.0' . "\r\n";
  $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
