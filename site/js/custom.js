633e63d7f3a97eaef380ff82b96ab2dcc406becc
Rom2016 <romaincharlier95@hotmail.com>
2016-12-11 19:50:53 +0100

    commit 2

---
+++ site/js/custom.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Rom on 11/12/2016.
+ */
+$(document).ready(function() {
+        $('#form').on('submit',function(event){
+            var rq = $('input[type="submit"]').attr('name');
+            event.preventDefault()
+            var formData = new FormData($(this)[0]);
+           // if(!testOk($(this),btn)) return false;
+            $.ajax({
+                url: './php/traiteForm.php?rq='+rq,
+                type: 'POST',
+                data: formData,
+                async: true,
+                cache: false,
+                contentType: false,
+                processData: false,
+                success: function (data) {
+                    $('.js').html(data);
+                    $('.js').fadeIn().delay(4000).fadeOut();
+
+                }
+            });
+        });
+})
+
