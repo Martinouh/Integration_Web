61acd5c7cab1fb5a801b8a89a894d4e6a6307851
Martin G <martinouh@localhost.localdomain>
2016-12-14 10:08:11 +0100

    index

--- site/email/index.php
+++ site/email/index.php
@@ -1,36 +1,50 @@
-<?php
- if(isset($_POST['submit']))
- {
-    $name = $_POST['name'];
-	$email = $_POST['email'];
-	$query = $_POST['message'];
-	$email_from = $name.'<'.$email.'>';
-
- $to="contact@easywaitingroom.be";
- $subject="Enquiry!";
- $headers  = 'MIME-Version: 1.0' . "\r\n";
- $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
- $headers .= "From: ".$email_from."\r\n";
- $message="
-
- 		 Name:
-		 $name
-         <br>
- 		 Email-Id:
-		 $email
-         <br>
- 		 Message:
-		 $query
-
-   ";
-
-	if(mail($to,$subject,$message,$headers)){
-		header("Location:../contact?msg=Message envoyé! Merci de nous avoir contacté.");
-    }
-	else{
-		header("Location:../contact?msg=Erreur lors de l'envoi du message!");
-        //contact:-your-email@your-domain.com
-    }
-
- }
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>404 Not Found</title>
+        <link href="https://fonts.googleapis.com/css?family=Courgette" rel="stylesheet">
+    </head>
+    <style>
+        body{
+            font-family: 'Courgette', cursive;
+            background-color: #f3f3e1;
+        }
+        #contenu{
+            width: auto;
+            height: auto;
+            vertical-align: middle;
+            text-align: center;
+        }
+        a{
+            color: white;
+            background: #8F8E8C;
+            text-decoration: none;
+            padding: 7px 120px;
+            font-size: 13px;
+            font-family: arial, serif;
+            font-weight: bold;
+            -webkit-border-radius: 3em;
+        }
+        #error{
+            color: rgb(228, 146, 162);
+            font-size: 20px;
+            margin-top: 1px;
+            text-align: center;
+        }
+        h1{
+            font-size: 200px;
+            color: #8F8E8C;
+            text-align: center;
+            margin-bottom: 1px;
+            text-shadow: 1px 1px 6px #fff;
+        }
+    </style>
+    <body>
+        <div id="contenu">
+            <h1>404</h1>
+            <p id="error">Error occurred! - File not Found</p>
+            <a href="../index.php">Back</a>
+        </div>
+    </body>
+</html>
